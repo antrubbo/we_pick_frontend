c2b145d119c7af510bb9405d950d261deb956a28
antrubbo <anthony.rubbo@gmail.com>
2021-02-11 11:55:10 -0500

    initial flex styling to explore, header

--- src/components/pages/Explore.js
+++ src/components/pages/Explore.js
@@ -7,16 +7,18 @@ function Explore({initialMovies, setDetailsMovieId, searchTerms, setSearchTerms,
     
     return (
         <div className="container">
+        {/* <> */}
             {searchResults ? <SearchResultsModal setDetailsMovieId={setDetailsMovieId} show={modalShow} onHide={() => setModalShow(false)} searchResults={searchResults}/> : null}
             <div className="welcome-container">
-                <h2>Less scroll - more watch!</h2>
+                <h2 id="welcome-h2">Less scroll - more watch!</h2>
                 <h4>Find a movie to watch without endless searching.</h4>
                 <Search searchTerms={searchTerms} setSearchTerms={setSearchTerms} handleSearch={handleSearch} />
             </div>
             <div className="carousel-div">
-                <h4>Today's most popular titles:</h4>
+                <h4 id="carousel-headline-h4">Today's most popular titles:</h4>
                 <MovieCarousel initialMovies={initialMovies} setDetailsMovieId={setDetailsMovieId}/>
             </div>
+            {/* </> */}
         </div>
     )
 }
